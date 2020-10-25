920c7ed3f8b10ca9e4900679114579b32c74b17f
daviddongguo <david.dong.guo@gmail.com>
2020-10-25 14:46:39 -0400

    Add moderate method in moderation sever to moderate all events when sever starts.

--- moderation/index.js
+++ moderation/index.js
@@ -4,10 +4,7 @@ const axios = require('axios');
 
 const app = express();
 app.use(bodyParser.json());
-
-app.post('/events', (req, res) => {
-	console.log('4003 received event : ' + req.body.type);
-	const {type, data} = req.body;
+const moderate = (type, data) => {
 	if (type === 'CommentCreated') {
 		const status = data.content.includes('orange') ? 'rejected' : 'approved';
 
@@ -21,9 +18,24 @@ app.post('/events', (req, res) => {
 			},
 		});
 	}
+};
+
+app.post('/events', (req, res) => {
+	//console.log('4003 received event : ' + req.body.type);
+	const {type, data} = req.body;
+	moderate(type, data);
+
 	res.send({});
 });
 
-app.listen(4003, () => {
+app.listen(4003, async () => {
 	console.log('Listening on 4003');
+
+	const res = await axios.get('http://localhost:4005/events');
+	console.log('4003 Received Events :');
+	for (let event of res.data) {
+		moderate(event.type, event.data);
+		console.log('4003 Processing event : ' + event.type);
+	}
+	console.log('end of for loop');
 });
