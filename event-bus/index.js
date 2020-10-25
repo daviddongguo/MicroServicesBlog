daa05935d5c36ff28fc655ecc868e39044925fa2
daviddongguo <david.dong.guo@gmail.com>
2020-10-25 14:26:25 -0400

    Store events by event-bus

--- event-bus/index.js
+++ event-bus/index.js
@@ -5,9 +5,13 @@ const axios = require('axios');
 const app = express();
 app.use(bodyParser.json());
 
+const events = [];
+
 app.post('/events', (req, res) => {
 	const event = req.body;
 	console.log('4005 Received Event : ' + event.type);
+
+	events.push(event);
 
 	axios.post('http://localhost:4000/events', event); // for post
 	axios.post('http://localhost:4001/events', event); // for comment
@@ -15,6 +19,10 @@ app.post('/events', (req, res) => {
 	axios.post('http://localhost:4003/events', event); // for moderation
 
 	res.send({status: 'OK'});
+});
+
+app.get('/events', (req, res) => {
+	res.send(events);
 });
 
 app.listen(4005, () => {
--- query/index.js
+++ query/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const axios = require('axios');
 
 const app = express();
 app.use(bodyParser.json());
@@ -8,17 +9,12 @@ app.use(cors());
 
 const posts = {};
 
-app.get('/posts', (req, res) => {
-	res.send(posts);
-});
-
-app.post('/events', (req, res) => {
-	const {type, data} = req.body;
-
+const handleEvent = (type, data) => {
 	if (type === 'PostCreated') {
 		const {id, title} = data;
 		const comments = [];
 		posts[id] = {id, title, comments};
+		console.log('Inside handleEvent :' + posts[id].title);
 	}
 	if (type === 'CommentCreated') {
 		const {id, content, postId, status} = data;
@@ -26,18 +22,35 @@ app.post('/events', (req, res) => {
 		post.comments.push({id, content, status});
 	}
 	if (type === 'CommentUpdated') {
-		console.log('4002 CommentUpdated: ' + data.status);
-		const {id, postId, content, status} = data;
+		console.log('4002 commentUpdated: ' + data.status);
+		const {id, postId: postId, content, status} = data;
 		const comment = posts[postId].comments.find((c) => {
 			return c.id === id;
 		});
 		comment.status = status;
 		comment.content = content;
 	}
+};
+
+app.get('/posts', (req, res) => {
+	res.send(posts);
+});
+
+app.post('/events', (req, res) => {
+	const {type, data} = req.body;
+	handleEvent(type, data);
 
 	res.send({});
 });
 
-app.listen(4002, () => {
+app.listen(4002, async () => {
 	console.log('Listening on 4002');
+
+	const res = await axios.get('http://localhost:4005/events');
+	console.log('4002 Received Events :');
+	for (let event of res.data) {
+		handleEvent(event.type, event.data);
+		console.log('4002 Processing event : ' + event.type);
+	}
+	console.log('end of for loop');
 });
